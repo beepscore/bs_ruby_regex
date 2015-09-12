20b1b7460f2129ae54a89096091b103aea93d3b3
beepscore <support@beepscore.com>
2015-09-12 00:03:23 -0700

    In bs_file_accessor delete attribute accessors and change methods to be functional class methods.
    Update bs_file_accessor_test to match.

--- lib/bs_ruby_regex/bs_file_accessor.rb
+++ lib/bs_ruby_regex/bs_file_accessor.rb
@@ -3,9 +3,6 @@
 module BsRubyRegex
   class BsFileAccessor
 
-    attr_reader :file_encoding
-    attr_reader :file_name
-
     def initialize
     end
 
@@ -23,18 +20,17 @@ module BsRubyRegex
     # http://www.ruby-doc.org/core-2.0.0/IO.html#method-c-new
     # http://www.ruby-doc.org/core-2.0.0/Encoding.html
     #
-    def string_from_file(file_name, an_external_encoding)
-      @file_name = file_name
+    def self.string_from_file(file_name, an_external_encoding)
 
       file = nil
       if (an_external_encoding.nil?)
         # don't specify external encoding, just use default
-        file = File.open(@file_name, 'r')
+        file = File.open(file_name, 'r')
       else
-        file = File.open(@file_name, "r:#{an_external_encoding}")
+        file = File.open(file_name, "r:#{an_external_encoding}")
       end
 
-      @file_encoding = file.external_encoding
+      #file_encoding = file.external_encoding
 
       file_string = ""
       file.each_line do |line|
@@ -45,7 +41,7 @@ module BsRubyRegex
       file_string
     end
 
-    def write(a_string, to_file)
+    def self.write(a_string, to_file)
       # References
       # Learn Ruby the Hard Way ex20.rb
       # http://stackoverflow.com/questions/4795447/rubys-file-open-and-the-need-for-f-close
@@ -54,6 +50,17 @@ module BsRubyRegex
       output.write(a_string)
       output.close
       puts "Wrote to file #{to_file}"
+    end
+
+    def self.encoding_from_file(file_name, an_external_encoding)
+      file = nil
+      if (an_external_encoding.nil?)
+        # don't specify external encoding, just use default
+        file = File.open(file_name, 'r')
+      else
+        file = File.open(file_name, "r:#{an_external_encoding}")
+      end
+      file.external_encoding
     end
 
   end
--- test/bs_file_accessor_test.rb
+++ test/bs_file_accessor_test.rb
@@ -11,7 +11,7 @@ class BsFileAccessorTest < MiniTest::Test
     @os = BsOsDetector.os
   end
 
-  def test_string_from_file_sets_file_encoding
+  def test_encoding_from_file_default
     # Terminal file command shows
     # $ file sample-input.txt
     # sample-input.txt: ASCII text
@@ -24,42 +24,42 @@ class BsFileAccessorTest < MiniTest::Test
     # $ file sample-input-utf8.txt
     # sample-input-utf8.txt: UTF-8 Unicode text
 
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    file_accessor.string_from_file('./sample-input.txt', nil)
+    encoding_from_file = BsRubyRegex::BsFileAccessor.encoding_from_file('./sample-input.txt', nil)
     expected_result = nil
     if (:windows == @os)
       expected_result = Encoding.find('IBM437')
     else
       expected_result = Encoding.find('UTF-8')
     end
-    assert_equal(expected_result, file_accessor.file_encoding)
-
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    file_accessor.string_from_file('./sample-input-utf8.txt', nil)
-    expected_result = nil
-    if (:windows == @os)
-      expected_result = Encoding.find('IBM437')
-    else
-      expected_result = Encoding.find('UTF-8')
-    end
-    assert_equal(expected_result, file_accessor.file_encoding)
-
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    file_accessor.string_from_file('./sample-input.txt', 'UTF-8')
-    expected_result = Encoding.find('UTF-8')
-    assert_equal(expected_result, file_accessor.file_encoding, "expect UTF-8 for any OS")
+    assert_equal(expected_result, encoding_from_file)
   end
 
-  def test_string_from_file_returns_string
+  def test_encoding_from_file_nil_utf_8
+    encoding_from_file = BsRubyRegex::BsFileAccessor.encoding_from_file('./sample-input-utf8.txt', nil)
+    expected_result = nil
+    if (:windows == @os)
+      expected_result = Encoding.find('IBM437')
+    else
+      expected_result = Encoding.find('UTF-8')
+    end
+    assert_equal(expected_result, encoding_from_file)
+  end
 
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    actual_result = file_accessor.string_from_file('./sample-input.txt', nil)
+  def test_encoding_from_file_utf_8
+    encoding_from_file = BsRubyRegex::BsFileAccessor.encoding_from_file('./sample-input.txt', 'UTF-8')
+    expected_result = Encoding.find('UTF-8')
+    assert_equal(expected_result, encoding_from_file, "expect UTF-8 for any OS")
+  end
+
+  def test_string_from_file_encoding_nil
+    actual_result = BsRubyRegex::BsFileAccessor.string_from_file('./sample-input.txt', nil)
     puts
     puts "file_string: #{actual_result}"
     assert_equal(GAMES_STRING_ASCII, actual_result)
+  end
 
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    actual_result = file_accessor.string_from_file('./sample-input-utf8.txt', 'UTF-8')
+  def test_string_from_file_encoding_utf_8
+    actual_result = BsRubyRegex::BsFileAccessor.string_from_file('./sample-input-utf8.txt', 'UTF-8')
     puts
     puts "file_string: #{actual_result}"
     assert_equal(GAMES_STRING_UTF8, actual_result)
@@ -72,8 +72,7 @@ class BsFileAccessorTest < MiniTest::Test
     end
     assert(!File.exists?('./junk.txt'))
 
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    file_accessor.write(GAMES_STRING_ASCII, './junk.txt')
+    BsRubyRegex::BsFileAccessor.write(GAMES_STRING_ASCII, './junk.txt')
 
     assert(File.exists?('./junk.txt'))
 
@@ -89,8 +88,7 @@ class BsFileAccessorTest < MiniTest::Test
     end
     assert(!File.exists?('./junk-utf8.txt'))
 
-    file_accessor = BsRubyRegex::BsFileAccessor.new
-    file_accessor.write(GAMES_STRING_UTF8, './junk-utf8.txt')
+    BsRubyRegex::BsFileAccessor.write(GAMES_STRING_UTF8, './junk-utf8.txt')
 
     assert(File.exists?('./junk-utf8.txt'))
 
